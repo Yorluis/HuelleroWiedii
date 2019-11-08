793d6c6765ca9ef46c52b0b7630024356eedee84
Yorluis Vega <yorluis.vega@gmail.com>
2019-11-08 15:14:14 -0500

    proyecto

---
+++ includes/footer.php
@@ -0,0 +1,57 @@
+           <div class="container-box">
+              <div class="container">
+                  <div class="widget">
+                        <div class="fecha">
+                          <p id="diaSemana" class="diaSemana"></p>
+                          <p id="dia" class="dia"></p>
+                          <p>de </p>
+                          <p id="mes" class="mes"></p>
+                          <p>del </p>
+                          <p id="year" class="year"></p>
+                        </div>
+                     </div>
+                 </div>
+ 
+
+              <div class="container2">
+                <div class="widget">
+                    <div class="reloj">
+                      <p id="horas" class="horas"></p>
+                      <p>:</p>
+                      <p id="minutos" class="minutos"></p>
+                      <p>:</p>
+                      <div class="caja-segundos">
+                          <p id="segundos" class="segundos"></p>
+                          <p id="ampm" class="ampm"></p>
+                      </div>
+                    </div>
+                </div>
+              </div> 
+
+              <div class="container1">
+                 <img class="img-wiedii" src= "./imagenes/wd.svg"></img>
+                
+                </div>
+
+  <script src="reloj.js"></script>
+    
+ 
+ 
+ <!-- SCRIPTS BOOTSTRAP-->
+
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+   integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+   crossorigin="anonymous"></script> 
+   
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
+   integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" 
+   crossorigin="anonymous"></script>
+
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
+   integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
+   crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
